26e4b2cff9fc82a4ce9a25f62c61c2c04da9354e
Роман Худенко <damage----------inc@yandex.ru>
2024-02-13 18:47:30 +0400

    added all base functionality

--- res/js/app.js
+++ res/js/app.js
@@ -1,15 +1,19 @@
-var username, y_map, editing_mark,
-    marks = [],
-    groups = [];
+var username, y_map, editing_mark, editing_group,
+        marks = [],
+        groups = [];
 
-var marks_modal = document.getElementById('marks_modal'),
+var add_mark_select = document.getElementById('add_mark_select'),
+    marks_modal = document.getElementById('marks_modal'),
     groups_modal = document.getElementById('groups_modal'),
+    groups_modal_bootstrap = new bootstrap.Modal(document.getElementById('groups_modal')),
     new_mark_modal = new bootstrap.Modal(document.getElementById('new_mark_modal')),
+    edit_mark_modal = new bootstrap.Modal(document.getElementById('edit_mark_modal')),
     choose_action_modal = new bootstrap.Modal(document.getElementById('choose_action_modal')),
     groups_tbody = document.getElementById('groups_tbody'),
-    groups_modal = document.getElementById('groups_modal'),
     new_group_modal = new bootstrap.Modal(document.getElementById('new_group_modal')),
-    edit_group_modal = new bootstrap.Modal(document.getElementById('edit_group_modal'));
+    edit_group_modal = new bootstrap.Modal(document.getElementById('edit_group_modal')),
+    group_marks_tbody = document.getElementById('group_marks_tbody'),
+    group_marks_modal = new bootstrap.Modal(document.getElementById('group_marks_modal'));
 
 marks_modal.addEventListener('shown.bs.modal', () => {
         load_user_marks();
@@ -20,13 +24,64 @@ groups_modal.addEventListener('shown.bs.modal', () => {
     }
 );
 
+function update_mark() {
+    'use strict';
+    var mark = editing_mark,
+        name = document.getElementById('edit_mark_name').value,
+        colorHex = document.getElementById('edit_mark_color').value,
+        data = {
+            'name': name,
+            'latitude': mark.data.latitude,
+            'longitude': mark.data.longitude,
+            'colorHex': colorHex.substring(1)
+        };
+        mark.data.name = name;
+        mark.data.colorHex = colorHex.substring(1);
+    y_map.geoObjects.remove(mark.ymap);
+    mark.ymap = new ymaps.Placemark(
+        [mark.data.longitude, mark.data.latitude],
+        {
+            iconCaption: name
+        }, {
+            preset: 'islands#icon',
+            iconColor: colorHex
+        }
+    );
+    mark.ymap.events.add('click', function () {
+        editing_mark = mark;
+        choose_action_modal.show();
+    });
+    y_map.geoObjects.add(mark.ymap);
+    fetch('/api/mark/' + mark.data.id, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    edit_mark_modal.hide();
+}
+
 function edit_mark() {
     'use strict';
     choose_action_modal.hide();
+    document.getElementById('edit_mark_lon').value = editing_mark.data.longitude;
+    document.getElementById('edit_mark_lat').value = editing_mark.data.latitude;
+    document.getElementById('edit_mark_name').value = editing_mark.data.name;
+    document.getElementById('edit_mark_color').value = "#" + editing_mark.data.colorHex;
+    edit_mark_modal.show();
 }
 
 function edit_mark_request(event) {
     'use strict';
+    var i;
+    for (i = 0; i < marks.length; i += 1) {
+        if (marks[i].data.id == event.target.getAttribute('data-id')) {
+            editing_mark = marks[i];
+            break;
+        }
+    }
+    edit_mark();
 }
 
 function is_has_mark_id(mark_id) {
@@ -234,6 +289,7 @@ function edit_group_request(event) {
     document.getElementById('edit_group_id').value = group.data.id;
     document.getElementById('edit_group_name').value = group.data.name;
     document.getElementById('edit_group_description').value = group.data.description;
+    editing_group = group;
     edit_group_modal.show();
 }
 
@@ -266,8 +322,41 @@ function delete_group_request(event) {
     }
 }
 
-function show_group_request() {
+function show_all_marks_request() {
     'use strict';
+    debug('show_all_marks_request');
+    var i;
+    for (i = 0; i < marks.length; i += 1) {
+        y_map.geoObjects.add(marks[i].ymap);
+    }
+    document.getElementById('show_all_marks_button').setAttribute('class', 'nav-item fade');
+}
+
+function show_group_request(event) {
+    'use strict';
+    debug('show_group_request');
+    groups_modal_bootstrap.hide();
+    document.getElementById('show_all_marks_button').setAttribute('class', 'nav-item');
+    load_get(
+        '/api/mark/in/' + event.target.getAttribute('data-id'),
+        function (data) {
+            var i, j, flag, arr = JSON.parse(data);
+            for (i = 0; i < marks.length; i += 1) {
+                flag = false;
+                for (j = 0; j < arr.length; j += 1) {
+                    if (marks[i].data.id == arr[j].id) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag) {
+                    y_map.geoObjects.add(marks[i].ymap);
+                } else {
+                    y_map.geoObjects.remove(marks[i].ymap);
+                }
+            }
+        }
+    );
 }
 
 function add_and_render_group(group_data) {
@@ -329,6 +418,100 @@ function add_and_render_group(group_data) {
         groups_tbody.appendChild(tr);
         groups[groups.length] = group_obj;
     }
+}
+
+function delete_mark_from_group_request(event) {
+    'use strict';
+    debug('delete_mark_from_group_request');
+    load_post_json(
+        '/api/rel/removeGroupFromMark',
+        {
+            'markId': event.target.getAttribute('data-id'),
+            'groupId': editing_group.data.id
+        },
+        function (resp) {
+            edit_group_marks();
+        }
+    );
+}
+
+function add_group_mark(mark_data) {
+    'use strict';
+    debug('add_group_mark');
+    var tr = document.createElement('tr'),
+        name_td = document.createElement('td'),
+        edit_div = document.createElement('div'),
+        edit_td = document.createElement('td'),
+        delete_but = document.createElement('button'),
+        delete_i = document.createElement('i');
+    name_td.appendChild(document.createTextNode(mark_data.name));
+    name_td.setAttribute('class', 'align-middle');
+    edit_td.setAttribute('class', 'd-flex flex-row-reverse');
+    tr.appendChild(name_td);
+    delete_i.setAttribute('class', 'bi bi-eraser-fill');
+    delete_i.setAttribute('data-id', mark_data.id);
+    delete_but.appendChild(delete_i);
+    delete_but.setAttribute('title', 'Удалить');
+    delete_but.setAttribute('class', 'btn btn-danger');
+    delete_but.setAttribute('type', 'button');
+    delete_but.setAttribute('data-id', mark_data.id);
+    delete_but.onclick = delete_mark_from_group_request;
+    edit_div.appendChild(delete_but);
+    edit_td.appendChild(edit_div);
+    tr.appendChild(edit_td);
+    group_marks_tbody.appendChild(tr);
+}
+
+function edit_group_marks() {
+    'use strict';
+    debug('edit_group_marks');
+    group_marks_tbody.innerHTML = '';
+    group_marks_modal.show();
+    load_get(
+        '/api/mark/in/' + editing_group.data.id,
+        function (data) {
+            var i, j, flag, option, arr = JSON.parse(data);
+            if (arr.length === 0) {
+                group_marks_tbody.innerHTML = '<tr><td>Пока тут пусто.</td></tr>';
+            }
+            add_mark_select.innerHTML = '';
+            for (i = 0; i < marks.length; i += 1) {
+                flag = true;
+                for (j = 0; j < arr.length; j += 1) {
+                    if (marks[i].data.id == arr[j].id) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    option = document.createElement('option');
+                    option.value = marks[i].data.id;
+                    option.appendChild(document.createTextNode(marks[i].data.name));
+                    add_mark_select.appendChild(option);
+                }
+            }
+            arr.forEach(
+                function (mark) {
+                    add_group_mark(mark);
+                }
+            );
+        }
+    );
+}
+
+function add_mark_to_group() {
+    'use strict';
+    debug('add_mark_to_group');
+    load_post_json(
+        '/api/rel/addGroupToMark',
+        {
+            'markId': add_mark_select.value,
+            'groupId': editing_group.data.id
+        },
+        function (resp) {
+            edit_group_marks();
+        }
+    );
 }
 
 function load_user_marks() {
@@ -479,7 +662,7 @@ function create_new_mark() {
         lon = document.getElementById('new_mark_lon').value,
         data = {
             'name': name,
-            'colorHex': color,
+            'colorHex': color.substring(1),
             'latitude': lat,
             'longitude': lon
         },
