f40ea6ff8e10e021108e32dc37949062de052b41
Роман Худенко <damage----------inc@yandex.ru>
2024-05-03 12:01:08 +0400

    Added actions mind map

---
+++ Map mark.mm
@@ -0,0 +1,101 @@
+<map version="freeplane 1.11.5">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Map mark" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1714721100536" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Управлять учётной записью" POSITION="bottom_or_right" ID="ID_1116914205" CREATED="1714721074538" MODIFIED="1714722533834" HGAP_QUANTITY="45.5 pt" VSHIFT_QUANTITY="275.99999 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="Создать аккаунт" POSITION="bottom_or_right" ID="ID_1963848274" CREATED="1714721965949" MODIFIED="1714722139345" HGAP_QUANTITY="56.75 pt">
+<node TEXT="По электронной почте" ID="ID_913437392" CREATED="1714722141019" MODIFIED="1714722154128"/>
+</node>
+<node TEXT="Войти" POSITION="bottom_or_right" ID="ID_950166645" CREATED="1714721122055" MODIFIED="1714722132083" HGAP_QUANTITY="61.25 pt" VSHIFT_QUANTITY="6.75 pt"/>
+<node TEXT="Выйти" ID="ID_719504656" CREATED="1714722404094" MODIFIED="1714722411220" HGAP_QUANTITY="63.5 pt"/>
+</node>
+<node TEXT="Отметить на карте" POSITION="bottom_or_right" ID="ID_52181454" CREATED="1714721159954" MODIFIED="1714722526491" HGAP_QUANTITY="44.75 pt" VSHIFT_QUANTITY="-296.24999 pt">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Создать метку" ID="ID_1597868373" CREATED="1714721324769" MODIFIED="1714722380599" HGAP_QUANTITY="65 pt" VSHIFT_QUANTITY="60.75 pt"/>
+<node TEXT="Найти метку" ID="ID_889457667" CREATED="1714722350308" MODIFIED="1714722383672" HGAP_QUANTITY="70.25 pt" VSHIFT_QUANTITY="10.5 pt"/>
+<node TEXT="Изменить метку" ID="ID_1488658010" CREATED="1714721907863" MODIFIED="1714721990251" HGAP_QUANTITY="68.75 pt" VSHIFT_QUANTITY="-8.25 pt">
+<node TEXT="Изменить общие данные" ID="ID_1658045985" CREATED="1714721992930" MODIFIED="1714722075514"/>
+<node TEXT="Переместить на карте" ID="ID_297149607" CREATED="1714722008750" MODIFIED="1714722021868"/>
+<node TEXT="Удалить" ID="ID_1853504282" CREATED="1714722014655" MODIFIED="1714722017155"/>
+</node>
+<node TEXT="Сгруппировать метки" ID="ID_1112431637" CREATED="1714721178249" MODIFIED="1714721960264" HGAP_QUANTITY="61.25 pt" VSHIFT_QUANTITY="-26.25 pt">
+<node TEXT="Добавить в группу" ID="ID_1998091732" CREATED="1714722085466" MODIFIED="1714722114298"/>
+<node TEXT="Удалить из группы" ID="ID_677005440" CREATED="1714722093140" MODIFIED="1714722124200"/>
+<node TEXT="Группы" ID="ID_585381844" CREATED="1714722164875" MODIFIED="1714722166862">
+<node TEXT="Создать" ID="ID_1223970757" CREATED="1714722168797" MODIFIED="1714722172208"/>
+<node TEXT="Изменить" ID="ID_909409634" CREATED="1714722176258" MODIFIED="1714722178301"/>
+<node TEXT="Удалить" ID="ID_1203178046" CREATED="1714722180644" MODIFIED="1714722186082"/>
+</node>
+</node>
+</node>
+</node>
+</map>
