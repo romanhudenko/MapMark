403dfc46cf6ab7cc861d4e2872f6fbfccf73cc62
Роман Худенко <damage----------inc@yandex.ru>
2024-05-14 11:21:58 +0400

    Login tests

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -14,5 +14,73 @@ public class AuthTest {
         $("#password").sendKeys("user");
         $("button[type=\"submit\"]").click();
         $x("//*[contains(text(),'Вы вошли как user')]").shouldBe(visible);
+        closeWebDriver();
+    }
+
+    @Test
+    public void login_not_existent() {
+        open("http://localhost:8080/");
+        $("#username").sendKeys("test");
+        $("#password").sendKeys("test");
+        $("button[type=\"submit\"]").click();
+        $("#error_block").shouldBe(visible);
+        closeWebDriver();
+    }
+
+    @Test
+    public void login_caps() {
+        open("http://localhost:8080/");
+        $("#username").sendKeys("USER");
+        $("#password").sendKeys("USER");
+        $("button[type=\"submit\"]").click();
+        $("#error_block").shouldBe(visible);
+        closeWebDriver();
+    }
+
+    @Test
+    public void empty_login() {
+        open("http://localhost:8080/");
+        $("#password").sendKeys("user");
+        $("button[type=\"submit\"]").click();
+        $("#error_block").shouldBe(visible);
+        closeWebDriver();
+    }
+
+    @Test
+    public void empty_password() {
+        open("http://localhost:8080/");
+        $("#username").sendKeys("user");
+        $("button[type=\"submit\"]").click();
+        $("#error_block").shouldBe(visible);
+        closeWebDriver();
+    }
+
+    @Test
+    public void spacebars_at_sides() {
+        open("http://localhost:8080/");
+        $("#username").sendKeys(" user ");
+        $("#password").sendKeys(" user ");
+        $("button[type=\"submit\"]").click();
+        $("#error_block").shouldBe(visible);
+        closeWebDriver();
+    }
+
+    @Test
+    public void sql_injection() {
+        open("http://localhost:8080/");
+        $("#username").sendKeys("a' or 1=1");
+        $("button[type=\"submit\"]").click();
+        $("#error_block").shouldBe(visible);
+        closeWebDriver();
+    }
+
+    @Test
+    public void long_texts() {
+        open("http://localhost:8080/");
+        $("#username").sendKeys("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+        $("#password").sendKeys("0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+        $("button[type=\"submit\"]").click();
+        $("#error_block").shouldBe(visible);
+        closeWebDriver();
     }
 }
