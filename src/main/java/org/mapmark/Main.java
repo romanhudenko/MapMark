df4c45a15505a59f4a83124b791c71196483f83a
Роман Худенко <romanhudenko370@gmail.com>
2024-09-05 20:47:08 +0400

    Added notation

--- src/main/java/org/mapmark/Main.java
+++ src/main/java/org/mapmark/Main.java
@@ -1,9 +1,10 @@
 package org.mapmark;
 
-
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@PropertySource("classpath:/application.properties")
 @SpringBootApplication
 public class        Main {
 
